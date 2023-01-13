7591b2614670a22b2773a120eb4b775502dc011b
Silvio Knizek <killermoehre@gmx.net>
2023-01-13 21:18:28 +0100

    add support for unlimited data passes

--- telekom_statistics.py
+++ telekom_statistics.py
@@ -16,25 +16,36 @@ def generate_text() -> str:
         with urllib.request.urlopen(API_ENDPOINT) as telekom_api:
             api_answer = json.load(telekom_api)
             connected = True
+        # if both initialVolume and usedVolume are not available, we are currently in an unlimited plan
+        if "initialVolume" not in api_answer:
+            api_answer["initialVolume"] = float("inf")
+        if "usedVolume" not in api_answer:
+            api_answer["usedVolume"] = float("inf")
     except (json.decoder.JSONDecodeError, urllib.error.URLError):
         # if the output can't be decoded, that means we don't have access to the API and are not in the Telekom mobile network.
-        connected = False
-        api_answer["icon"] = "network-wireless-disconnected"
         api_answer["availableVolumePercent"] = 0
+        api_answer["icon"] = "network-wireless-disconnected"
         api_answer["initialVolume"] = 0
         api_answer["usedVolume"] = 0
-    # convert the nested dict to an object
-    api_answer["initialVolumeShort"] = api_answer["initialVolume"] / 1024**3
-    api_answer["usedVolumeShort"] = api_answer["usedVolume"] / 1024**3
-    api_answer["remainingVolumeShort"] = (
-        api_answer["initialVolume"] - api_answer["usedVolume"]
-    ) / 1024**3
+        connected = False
+
     try:
         api_answer["availableVolumePercent"] = (
             1 - api_answer["usedVolume"] / api_answer["initialVolume"]
         ) * 100
     except ZeroDivisionError:
         api_answer["availableVolumePercent"] = 0
+    if api_answer["initialVolume"] == float("inf"):
+        api_answer["initialVolumeShort"] = "♾"
+        api_answer["remainingVolumeShort"] = "♾"
+        api_answer["availableVolumePercent"] = 100
+    else:
+        api_answer[
+            "initialVolumeShort"
+        ] = f'{api_answer["initialVolume"] / 1024**3}:.2f'
+        api_answer[
+            "remainingVolumeShort"
+        ] = f'{(api_answer["initialVolume"] - api_answer["usedVolume"]) / 1024**3}:.2f'
 
     api_answer["icon"] = "network-wireless-connected-00"
     api_answer["colour"] = "red"
@@ -58,7 +69,7 @@ def generate_text() -> str:
     if connected:
         return_string += f"{api_answer['passName']}\n"
         return_string += "<small>Ihr verbleibendes Datenvolumen</small>\n\n"
-        return_string += f"<span foreground='{api_answer['colour']}'>{api_answer['remainingVolumeShort']:.2f}</span>/{api_answer['initialVolumeShort']:.2f} GiB\n"
+        return_string += f"<span foreground='{api_answer['colour']}'>{api_answer['remainingVolumeShort']}</span>/{api_answer['initialVolumeShort']} GiB\n"
         return_string += (
             f"<small>verbleibend für {api_answer['remainingTimeStr']}</small>"
         )
