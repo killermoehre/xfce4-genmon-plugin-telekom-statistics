7e1b3c98e1bb6d2848d5c2f4e6a0db6a0de4c405
Silvio Knizek <killermoehre@gmx.net>
2023-01-13 17:22:35 +0100

    handle the case with no network connection at all

--- telekom_statistics.py
+++ telekom_statistics.py
@@ -4,44 +4,46 @@ import json
 import urllib.request
 import urllib.error
 
-PASS_HOST = "https://pass.telekom.de"
-API_ENDPOINT = f"{PASS_HOST}/api/service/generic/v1/status"
+PASS_HOST = "pass.telekom.de"
+API_ENDPOINT = f"https://{PASS_HOST}/api/service/generic/v1/status"
 
 
 def generate_text() -> str:
     """Output mimicking PASS_HOST. See https://docs.xfce.org/panel-plugins/xfce4-genmon-plugin/start for styling information"""
-    with urllib.request.urlopen(API_ENDPOINT) as telekom_api:
-        connected: bool
-        api_answer: dict = {}
-        try:
+    connected: bool
+    api_answer: dict = {}
+    try:
+        with urllib.request.urlopen(API_ENDPOINT) as telekom_api:
             api_answer = json.load(telekom_api)
             connected = True
-            # convert the nested dict to an object
-            api_answer["initialVolumeShort"] = api_answer["initialVolume"] / 1024**3
-            api_answer["usedVolumeShort"] = api_answer["usedVolume"] / 1024**3
-            api_answer["remainingVolumeShort"] = (
-                api_answer["initialVolume"] - api_answer["usedVolume"]
-            ) / 1024**3
-            api_answer["availableVolumePercent"] = (
-                1 - api_answer["usedVolume"] / api_answer["initialVolume"]
-            ) * 100
+    except (json.decoder.JSONDecodeError, urllib.error.URLError):
+        # if the output can't be decoded, that means we don't have access to the API and are not in the Telekom mobile network.
+        connected = False
+        api_answer["icon"] = "network-wireless-disconnected"
+        api_answer["availableVolumePercent"] = 0
+        api_answer["initialVolume"] = 1
+        api_answer["usedVolume"] = 0
+    # convert the nested dict to an object
+    api_answer["initialVolumeShort"] = api_answer["initialVolume"] / 1024**3
+    api_answer["usedVolumeShort"] = api_answer["usedVolume"] / 1024**3
+    api_answer["remainingVolumeShort"] = (
+        api_answer["initialVolume"] - api_answer["usedVolume"]
+    ) / 1024**3
+    api_answer["availableVolumePercent"] = (
+        1 - api_answer["usedVolume"] / api_answer["initialVolume"]
+    ) * 100
 
-            api_answer["icon"] = "network-wireless-connected-00"
-            api_answer["colour"] = "red"
-            if api_answer["availableVolumePercent"] >= 25:
-                api_answer["icon"] = "network-wireless-connected-25"
-                api_answer["colour"] = "orange"
-            if api_answer["availableVolumePercent"] >= 50:
-                api_answer["icon"] = "network-wireless-connected-50"
-                api_answer["colour"] = "yellow"
-            if api_answer["availableVolumePercent"] >= 75:
-                api_answer["icon"] = "network-wireless-connected-75"
-                api_answer["colour"] = "green"
-        except json.decoder.JSONDecodeError:
-            # if the output can't be decoded, that means we don't have access to the API and are not in the Telekom mobile network.
-            connected = False
-            api_answer["icon"] = "network-wireless-disconnected"
-            api_answer["availableVolumePercent"] = 0
+    api_answer["icon"] = "network-wireless-connected-00"
+    api_answer["colour"] = "red"
+    if api_answer["availableVolumePercent"] >= 25:
+        api_answer["icon"] = "network-wireless-connected-25"
+        api_answer["colour"] = "orange"
+    if api_answer["availableVolumePercent"] >= 50:
+        api_answer["icon"] = "network-wireless-connected-50"
+        api_answer["colour"] = "yellow"
+    if api_answer["availableVolumePercent"] >= 75:
+        api_answer["icon"] = "network-wireless-connected-75"
+        api_answer["colour"] = "green"
 
     return_string: str = "<txt>Telekom</txt>"
     return_string += f"<txtclick>exo-open {PASS_HOST}</txtclick>"
